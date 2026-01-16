ec2e81f9dc1623edb6da6cfbb3b3f6837e41f869
Akhil779200 <97981051+Akhil779200@users.noreply.github.com>
2026-01-16 18:45:56 +0000

    Add JavaScript learning files and dev container setup

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,29 @@
+const id = "1234"
+
+console.log("user id is: " + id)
+console.log(`id of user is ${id}`)
+
+let account_id = 100
+let Email = "harry@gmail.com"
+let accountemail = "qqq@gmail.com"
+let accountpassword = "12234"
+let accountcity = "jaipur"
+
+console.log(Email)
+console.log(`city of user is ${accountcity}`);
+console.log("account id is: " + accountcity)
+
+let accountema;
+
+account_id = 200
+ Email= "harry@gmai.com"
+accountemail = "qqq@gmil.com"
+ accountpassword= "124"
+ "use strict";
+console.table({account_id, Email, accountemail, accountpassword, accountcity, accountema})
+
+console.log(typeof account_id)
+
+
+
+
